c391fb7f9f862a594e0907b4b3915389f95d506a
louisbl <louis@beltramo.me>
2016-01-20 17:27:14 +0100

    Add main activity

---
+++ app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
@@ -0,0 +1,10 @@
+package fr.louisbl.hellocrm15;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by louis on 20/01/16.
+ * The Main Activity
+ */
+public class MainActivity extends AppCompatActivity {
+}
