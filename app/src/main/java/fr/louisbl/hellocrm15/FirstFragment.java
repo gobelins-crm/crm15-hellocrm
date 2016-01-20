93c2737b165fe4d1d64978f215415723980bf430
louisbl <louis@beltramo.me>
2016-01-20 18:47:28 +0100

    Click on fragment button

--- app/src/main/java/fr/louisbl/hellocrm15/FirstFragment.java
+++ app/src/main/java/fr/louisbl/hellocrm15/FirstFragment.java
@@ -3,9 +3,11 @@ package fr.louisbl.hellocrm15;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +15,8 @@ import android.view.ViewGroup;
  */
 public class FirstFragment extends Fragment {
 
+
+    private static final String TAG = "First Fragment";
 
     public FirstFragment() {
         // Required empty public constructor
@@ -23,7 +27,21 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button button = (Button) view.findViewById(R.id.fragmentButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "on Button clicked");
+            }
+        });
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
