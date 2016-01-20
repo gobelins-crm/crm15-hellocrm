3b6418d0ba27f9717ce7490b7836f9916f842200
louisbl <louis@beltramo.me>
2016-01-20 19:08:23 +0100

    Add custom ActionBar

--- app/src/main/java/fr/louisbl/hellocrm15/FragmentActivity.java
+++ app/src/main/java/fr/louisbl/hellocrm15/FragmentActivity.java
@@ -2,6 +2,7 @@ package fr.louisbl.hellocrm15;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class FragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.container_view);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager().beginTransaction().add(R.id.container, new FirstFragment()).commit();
     }
