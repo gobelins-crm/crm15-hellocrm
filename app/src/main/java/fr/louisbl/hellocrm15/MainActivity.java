9f44f808d9f92dc5c4e9b77cdd310685c818c0f9
louisbl <louis@beltramo.me>
2016-01-20 18:04:09 +0100

    Pass string extra between activity

--- app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
+++ app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by louis on 20/01/16.
@@ -19,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.main_activity);
 
+        final TextView myTextView = (TextView) findViewById(R.id.MyTextView);
+
         Button myButton = (Button) findViewById(R.id.MyButton);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("TextValue", myTextView.getText());
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/fr/louisbl/hellocrm15/SecondActivity.java
+++ app/src/main/java/fr/louisbl/hellocrm15/SecondActivity.java
@@ -1,13 +1,23 @@
 package fr.louisbl.hellocrm15;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String textValue = intent.getStringExtra("TextValue");
+
+        Log.d(TAG, textValue);
+
         setContentView(R.layout.activity_second);
     }
 }
