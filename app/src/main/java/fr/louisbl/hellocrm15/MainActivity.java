97f49812d7fa2663848876850640c11e467cab7a
louisbl <louis@beltramo.me>
2016-01-20 17:45:59 +0100

    Add onClick listener

--- app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
+++ app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
@@ -2,16 +2,24 @@ package fr.louisbl.hellocrm15;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by louis on 20/01/16.
  * The Main Activity
  */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
+    }
+
+    public void buttonClicked(View view) {
+        Log.d(TAG, "button clicked");
     }
 }
