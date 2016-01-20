8203363cd62c9564107e6d4e41fd4b08c4dcd3c6
louisbl <louis@beltramo.me>
2016-01-20 17:49:38 +0100

    Use clickListener

--- app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
+++ app/src/main/java/fr/louisbl/hellocrm15/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by louis on 20/01/16.
@@ -17,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
+
+        Button myButton = (Button) findViewById(R.id.MyButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "button clicked");
+            }
+        });
     }
 
     public void buttonClicked(View view) {
